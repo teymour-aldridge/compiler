2f5830e9338da352d3628c00ffe5518b68ed90b5
teymour-aldridge <notifications@reasoning.page>
2021-11-06 15:21:49 +0000

    Bump Cranelift dependencies.

--- logic/src/codegen/make_module.rs
+++ logic/src/codegen/make_module.rs
@@ -1,4 +1,4 @@
-use cranelift_codegen::{isa::BackendVariant, settings};
+use cranelift_codegen::settings;
 use cranelift_module::default_libcall_names;
 use cranelift_native::builder_with_options;
 use cranelift_object::{ObjectBuilder, ObjectModule};
@@ -6,9 +6,7 @@ use cranelift_object::{ObjectBuilder, ObjectModule};
 pub(crate) fn make_module_for_compiler_host_architecture() -> ObjectModule {
     let flags = settings::Flags::new(settings::builder());
 
-    let target = builder_with_options(BackendVariant::Any, true)
-        .unwrap()
-        .finish(flags);
+    let target = builder_with_options(true).unwrap().finish(flags);
 
     let builder = ObjectBuilder::new(target, vec![], default_libcall_names()).unwrap();
 
