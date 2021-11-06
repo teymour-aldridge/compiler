037889f40f8db166540f4ed61e670c0ee33d32fd
teymour-aldridge <notifications@reasoning.page>
2021-11-06 15:54:59 +0000

    Add some tests for simple programs.

---
+++ scripts/output.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+# tests that the files in `examples` provide the correct output when run
+
+# should be run from the root directory
+cargo build --workspace
+mkdir runin
+cd runin
+
+function cleanup() {
+    cd ..
+    rm -rf runin
+}
+
+trap cleanup EXIT
+
+# copy the runtime and compiler artefacts into this directory
+cp ../target/debug/libruntime.a libruntime.a
+cp ../target/debug/cli cli
+# copy the examples into this directory
+mkdir examples
+cp ../examples/*.pseudo examples
+
+for f in examples/*; do
+    ./cli "$f"
+    cc program.o libruntime.a
+    echo $(./a.out) >> output
+    echo $(cat "../examples/expected/$f") >> oracle
+    cmp output oracle
+done
