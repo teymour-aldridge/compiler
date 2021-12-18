be59377b08305e29daebb70370b12e7ac1067921
teymour-aldridge <notifications@reasoning.page>
2021-12-18 21:27:05 +0000

    Add additional documentation.

--- logic/src/codegen/mod.rs
+++ logic/src/codegen/mod.rs
@@ -6,8 +6,13 @@ use self::compile::Compiler;
 mod compile;
 /// Layouts of objects in memory.
 pub(self) mod layout;
+/// Produces the `ObjectModule` necessary for the compiler target in question.
 pub(self) mod make_module;
 
+/// Compiles the AST to machine code and writes it to an object file called `program.o`.
+///
+/// todo: automatically link
+/// todo: allow custom file outputs
 pub fn compile<'compiler>(ast: &'compiler TaggedAst, env: &'compiler TyEnv) {
     let mut compiler = Compiler::new(env);
 
