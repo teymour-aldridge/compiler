300db99513d6e7ec501e508cf78abdf841bc19cb
teymour-aldridge <notifications@reasoning.page>
2021-12-28 20:23:30 +0000

    Fix compilation on MacOS.
    The error previously was (on one line)
    ```
    dyld: BIND_OPCODE_SET_SEGMENT_AND_OFFSET_ULEB has segment 1
    which is not writable in /<redacted>/examples/./a.out
    ```
    
    The issue was that Cranelift is configured by default not to use PIC
    (position-independent code). This causes problems because MacOS
    does not seem to like the use of absolute addresses. Changing this
    configuration resolved the bug.

--- logic/src/codegen/make_module.rs
+++ logic/src/codegen/make_module.rs
@@ -1,4 +1,4 @@
-use cranelift_codegen::settings;
+use cranelift_codegen::settings::{self, Configurable};
 use cranelift_module::default_libcall_names;
 use cranelift_native::builder_with_options;
 use cranelift_object::{ObjectBuilder, ObjectModule};
@@ -8,7 +8,10 @@ use cranelift_object::{ObjectBuilder, ObjectModule};
 ///
 /// Cross-compiling is planned, but not yet supported.
 pub(crate) fn make_module_for_compiler_host_architecture() -> ObjectModule {
-    let flags = settings::Flags::new(settings::builder());
+    let mut builder = settings::builder();
+    builder.set("is_pic", "true").unwrap();
+
+    let flags = settings::Flags::new(builder);
 
     let target = builder_with_options(true).unwrap().finish(flags);
 
