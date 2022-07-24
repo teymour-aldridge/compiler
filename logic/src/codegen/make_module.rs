a6f608df4044921103043b308680122a19858a02
teymour-aldridge <teymour@reasoning.page>
2022-07-24 16:51:05 +0100

    Update cranelift to most recent version.

--- logic/src/codegen/make_module.rs
+++ logic/src/codegen/make_module.rs
@@ -13,7 +13,7 @@ pub(crate) fn make_module_for_compiler_host_architecture() -> ObjectModule {
 
     let flags = settings::Flags::new(builder);
 
-    let target = builder_with_options(true).unwrap().finish(flags);
+    let target = builder_with_options(true).unwrap().finish(flags).unwrap();
 
     let builder = ObjectBuilder::new(target, vec![], default_libcall_names()).unwrap();
 
